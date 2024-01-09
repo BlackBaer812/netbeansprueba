894d920de096d6bcfee66bfb58f95cd89803e940
usuario <usuario@Marcos.mshome.net>
2024-01-09 10:49:53 +0100

    Segundo commit, añadiendo neatbeansGitPrueba

---
+++ netbeansGitPrueba/src/main/java/com/proyecto/netbeansgitprueba/NetbeansGitPrueba.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.proyecto.netbeansgitprueba;
+
+/**
+ *
+ * @author usuario
+ */
+public class NetbeansGitPrueba {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Adios mundo");
+    }
+}
